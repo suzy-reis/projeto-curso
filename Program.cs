f24abbb72b366cc407c9598a2835d7f0599df18c
suzani reis <suzysureis@gmail.com>
2020-06-11 03:25:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int  A, B, Prod;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            Prod = A * B;
+
+
+            Console.WriteLine("PROD = " + Prod);
+            
+                        Console.ReadLine();
+
+
+        }
+    }
+}
